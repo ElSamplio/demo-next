6592fa84af6de8e78969e1df009f929d66aefbd4
Sergio Martinez <elsamplio@msn.com>
2024-11-11 17:44:33 -0500

    Update page.tsx
    Styled page

--- src/app/my-info/page.tsx
+++ src/app/my-info/page.tsx
@@ -3,8 +3,9 @@ import RootLayout from "../layout";
 const MyInfo: React.FC = () => {
   return (
     <RootLayout>
-      <div>
+      <div style={{marginTop: 50, marginLeft: 50}}>
         <h1>This page will look nice</h1>
+        <h3>Maybe now will look better</h3>
       </div>
     </RootLayout>
   );
