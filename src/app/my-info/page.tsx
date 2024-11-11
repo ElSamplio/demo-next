c9da36b2d53b5e150540107af2f03e6a90b3401e
Sergio Martinez <elsamplio@msn.com>
2024-11-11 17:23:18 -0500

    First load

---
+++ src/app/my-info/page.tsx
@@ -0,0 +1,13 @@
+import RootLayout from "../layout";
+
+const MyInfo: React.FC = () => {
+  return (
+    <RootLayout>
+      <div>
+        <h1>This page will look nice</h1>
+      </div>
+    </RootLayout>
+  );
+};
+
+export default MyInfo;
